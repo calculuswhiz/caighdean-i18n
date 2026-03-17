482830d72eb7bdf5bc9cd06d99c47792864bce6b
Zachary Bian <calculuswhiz@gmail.com>
2026-03-16 22:31:58 -0400

    Inlines done for 11, circling back on 10

--- scripts/bmlet-fgbLookup.js
+++ scripts/bmlet-fgbLookup.js
@@ -2,8 +2,11 @@ javascript:(() => {
 	/* Bookmarklet to look up selected word in FGB*/
 	const source = "https://www.teanglann.ie/en/fgb/";
 	const selection = window.getSelection();
-	const word = selection.toString().trim();
+	let word = selection.toString().trim();
 	if (word) {
+		if (word[1] === 'h' && confirm('De-lenite?')) {
+			word = word[0] + word.slice(2);
+		}
 		const url = `${source}${encodeURIComponent(word)}`;
 		window.open(url, "_blank");
 	}
