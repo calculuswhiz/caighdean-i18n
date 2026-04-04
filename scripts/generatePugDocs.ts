939adf722f77a6d4e8eed803e53d4951332e13c1
Zachary Bian <calculuswhiz@gmail.com>
2026-04-04 14:10:53 -0400

    Chapter 5 migrated

--- scripts/generatePugDocs.ts
+++ scripts/generatePugDocs.ts
@@ -11,31 +11,68 @@ const translationRoot = "./translation-pug";
 
 /** Scan chapters and languages, compile Pug files, and write to entrypoints. */
 async function generatePugDocs() {
-  const chapterFolders = (await fs.readdir(translationRoot, { withFileTypes: true }))
-    .filter(ent => ent.isDirectory() && ent.name !== "common");
-  for (const chapterFolder of chapterFolders) {
-    // EN, GA, etc
-    const availableLangs = (await fs.readdir(
-      path.join(translationRoot, chapterFolder.name, "dbs")
-    )).filter(file => file !== "index.pug").map(
-      file => path.parse(file).name
-        .toUpperCase()
-        .replace(/\.pug$/, "")
-    );
+  if (process.argv.includes("--watch")) {
+    console.log("Watching for changes...");
+    const watcher = fs.watch(translationRoot, { recursive: true });
+    for await (const event of watcher) {
+      if (event.filename == null || !event.filename.endsWith(".pug"))
+        continue;
 
-    for (const lang of availableLangs) {
-      const layoutPath = path.join(translationRoot, chapterFolder.name, "layout.pug");
-      const result = pug.renderFile(layoutPath, {
-        docLang: lang,
-        pretty: true
-      });
-      await fs.writeFile(
-        `./entrypoints/${lang}/${chapterFolder.name.toLocaleLowerCase()}-pug.html`,
-        result,
-        "utf-8"
+      console.log(`File changed: ${event.filename}`);
+
+      // Regenerate the document for the changed chapter
+      const chapterFolder = path.dirname(event.filename);
+      // EN, GA, etc
+      const availableLangs = (await fs.readdir(
+        path.join(translationRoot, chapterFolder, "dbs")
+      )).filter(file => file !== "index.pug").map(
+        file => path.parse(file).name
+          .toUpperCase()
+          .replace(/\.pug$/, "")
       );
+
+      for (const lang of availableLangs) {
+        const layoutPath = path.join(translationRoot, chapterFolder, "layout.pug");
+        // console.log(layoutPath);
+        try {
+          const result = pug.renderFile(layoutPath, { docLang: lang, pretty: true });
+          await fs.writeFile(
+            `./entrypoints/${lang}/${chapterFolder.toLocaleLowerCase()}-pug.html`,
+            result,
+            "utf-8"
+          );
+        } catch (err) {
+          console.error((err as Error).message);
+          continue;
+        }
+      }
+    }
+  } else {
+    const chapterFolders = (await fs.readdir(translationRoot, { withFileTypes: true }))
+      .filter(ent => ent.isDirectory() && ent.name !== "common");
+    for (const chapterFolder of chapterFolders) {
+      // EN, GA, etc
+      const availableLangs = (await fs.readdir(
+        path.join(translationRoot, chapterFolder.name, "dbs")
+      )).filter(file => file !== "index.pug").map(
+        file => path.parse(file).name
+          .toUpperCase()
+          .replace(/\.pug$/, "")
+      );
+
+      for (const lang of availableLangs) {
+        const layoutPath = path.join(translationRoot, chapterFolder.name, "layout.pug");
+        // console.log(layoutPath);
+        const result = pug.renderFile(layoutPath, { docLang: lang, pretty: true });
+        await fs.writeFile(
+          `./entrypoints/${lang}/${chapterFolder.name.toLocaleLowerCase()}-pug.html`,
+          result,
+          "utf-8"
+        );
+      }
     }
   }
+
 }
 
 generatePugDocs().catch(err => {
